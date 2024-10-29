9a578a826ab3cafbf928b1486e6e5e81f76e5043
Daz <shoeshinesamurai@gmail.com>
2024-10-29 00:26:02 -0700

    crud app

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-Route::get('/', function () {
-    return "API";
-});
+Route::apiResource('posts', PostController::class);
